8fabaf1070b88eb0d979117ccdf55945a296553f
Jo2831 <yosephman76@gmail.com>
2022-04-07 13:09:33 -0700

    ohh more

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include<unistd.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: print a quote using write function
+ *     *    ssize_t write(int fd, const void *buf, size_t count);
+ *      *
+ *       * Return: This time we return an error 1.
+ *       */
+
+int main(void)
+{
+		char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+		write(1, quo, 59);
+		return (1);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: using sizeof to print the size of various types.
- *     *
- *      * Return: Always 0 (Succesful)
- *      */
+ * main - Entry point
+ *
+ * Description; using sizeof to print the size of various types.
+ *
+ * Return: Always 0 (succesful)
+ */
 
 int main(void)
 {
-
-		/* sizeof evaluates the size of a variable */
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of an double: %lu byte(s)\n", sizeof(double));
-        printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size ofa float: %lu byte(s)\n", sizeof(float));
-
+	/*sizeof evaluates the size of a variable */
+	printf("Size of a char; %lu byte(s)\n",sizeof(char));
+	printf("Size of a int; %lu byte(s)\n",sizeof(int));
+	printf("Size of an double: %lu byte(s)\n",sizeof(double));
+	printf("Size of an long int; %lu byte(s)\n", sizeof(long int));
+	printf("Size of an long long int; %lu byte(s)\n",sizeof(long long int ));
+	
 	return (0);
+	
 }
+
