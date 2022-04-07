32ea502a643221631b2248d2378bc32a9ba8582a
Jo2831 <yosephman76@gmail.com>
2022-04-07 13:39:46 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,17 @@
-#include<unistd.h>
+#include <unistd.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: print a quote using write function
- *     *    ssize_t write(int fd, const void *buf, size_t count);
- *      *
- *       * Return: This time we return an error 1.
- *       */
+ * main - Entry point
+ *
+ * Descripton; print a quote using write function
+ *    ssize_t write(int fd, const void *buf, size_t count);
+ *
+ *    Return:This time we return an error 1.
+ */
 
 int main(void)
 {
-		char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-		write(1, quo, 59);
-		return (1);
-}
+	char quo[] = "and that piece of art is usefull\" -
+		Dora Koraparr, 2015-19-19\n"
+	write(1,quo,59);
+	return (1);
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,9 @@ int main(void)
 	printf("Size of a char; %lu byte(s)\n",sizeof(char));
 	printf("Size of a int; %lu byte(s)\n",sizeof(int));
 	printf("Size of an double: %lu byte(s)\n",sizeof(double));
-	printf("Size of an long int; %lu byte(s)\n", sizeof(long int));
-	printf("Size of an long long int; %lu byte(s)\n",sizeof(long long int ));
+	printf("Size of a long int; %lu byte(s)\n", sizeof(long int)); 
+	printf("Size of a long long int; %lu byte(s)\n",sizeof(long long int));
+	printf("Size of a float; %lu byte(s)\n",sizeof(float));
 	
 	return (0);
 	
