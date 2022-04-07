982ef4f8d1eb2c9aba341ed3c0f1e39660033d21
Jo2831 <yosephman76@gmail.com>
2022-04-07 07:36:42 -0700

    opps

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-
-/**
- *  * main - Entry point
- *   *
- *    * Description: A C program that prints with put function
- *     *
- *      * Return: Always 0 (Success)
- *      */
+/** 
+ * main-entry points
+ */
 
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+puts("\"programing is like building a muliitlingual puzzle");
+return (0);
 }
