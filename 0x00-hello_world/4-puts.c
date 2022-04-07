320bcf0b3129063d8208705d2fbd7d84f5ff4f57
Jo2831 <yosephman76@gmail.com>
2022-04-07 07:15:50 -0700

    hello_josi

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: A C program that prints with put function
+ *     *
+ *      * Return: Always 0 (Success)
+ *      */
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
