003abf669dab7a6346fb899ab5c5c6c137d23927
Jo2831 <jo2831@github.com>
2022-04-08 11:33:14 -0800

    nice school

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,8 +21,8 @@ int main(void)
 
 	if (n > 0)
 		printf("%i is positive\n", n);
-	else if (n == 0) 
-		printf("%i is zero\n", n);
+	else if (n == 0)	
+	printf("%i is zero\n", n);
 	else
 		printf("%i is negative\n", n);
 
