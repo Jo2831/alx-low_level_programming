ba1781974bb33c561d5165ad29a581af332bf843
Jo2831 <jo2831@github.com>
2022-04-08 12:49:09 -0800

    nice school

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,19 @@
 #include<unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-	char putchar (char c)
+	 putchar (char c)
 	{
 		write(1, &c, 1);
+		return (0);
 	}
 
 	char alpha;
