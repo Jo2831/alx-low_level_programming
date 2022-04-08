08f4341014df1d6e69f733528de1929b28084b24
Jo2831 <jo2831@github.com>
2022-04-08 12:52:40 -0800

    nice school

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,4 @@
-#include<unistd.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
@@ -10,11 +10,6 @@
 
 int main(void)
 {
-	 putchar (char c)
-	{
-		write(1, &c, 1);
-		return (0);
-	}
 
 	char alpha;
 	alpha = 'a';
