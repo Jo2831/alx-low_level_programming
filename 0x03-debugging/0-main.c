be17810c71240acdc2626ea1d196fdb496d7b3dc
YOSEPH DESALEGN <90330224+Jo2831@users.noreply.github.com>
2022-04-13 11:04:50 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include"main.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 98 % 2;
-        positive_or_negative(i);
+	i = 98 % 2;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
