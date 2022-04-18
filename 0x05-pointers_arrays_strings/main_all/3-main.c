b5ebf7cfcd12f1557470a1e026e73c7c5fb082b0
Jo2831 <jo2831@github.com>
2022-04-18 01:19:09 -0800

    pointer

--- 0x05-pointers_arrays_strings/main_all/3-main.c
+++ 0x05-pointers_arrays_strings/main_all/3-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
@@ -9,10 +8,8 @@
 int main(void)
 {
     char *str;
-    int len;
 
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
     return (0);
 }
