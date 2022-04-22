14c61b1b8c837ce4abaf77a71b585f8882092a85
yonatan65 <yonatankefyalew65@gmail.com>
2022-04-22 21:37:26 +0300

    msg

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN__
+# define MAIN__
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -8,3 +11,6 @@ char *cap_string(char *s);
 char *leet(char *s);
 char *rot13(char *s);
 void print_number(int n);
+int _putchar(char c);
+
+#endif
