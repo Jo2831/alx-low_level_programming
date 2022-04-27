ba76f5aa991e575fc3f9224badd74ff90489a899
Jo2831 <jo2831@github.com>
2022-04-27 00:23:35 -0800

    start recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,11 @@
 #include "main.h"
 void _puts_recursion(char *s)
 {
-_putchar(*s);
-s++;
 if (*s == '\0')
 {
 _putchar('\n');
 return;
 }
-_puts_recursion(s);
-
+_putchar(*s);
+_puts_recursion(s + 1);
 }
