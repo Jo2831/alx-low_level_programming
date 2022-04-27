cb4680bdedfe8bad36b81118287ce12a707ba1ba
Jo2831 <jo2831@github.com>
2022-04-27 00:52:13 -0800

    start recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,21 @@
-#include"main.h"
+/**
+ *_print_rev_recursion - Write a function that prints a string in reverse
+ * @s: string to print
+ *
+ * return: void
+ * @version 0.1
+ * @date 2022-04-27
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        return;
-    }
-    _print_rev_recursion(s + 1);
-    _putchar(*s);
+
+if (*s == '\0')
+{
+return;
+}
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
