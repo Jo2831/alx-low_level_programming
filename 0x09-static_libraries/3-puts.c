75093bd20b9df157a27f7920ef32d19c31a7af8c
Jo2831 <jo2831@github.com>
2022-04-28 23:13:02 -0800

    msk

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ *
+ * @str: string pointer
+ *
+ * Return: nothing
+*/
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] >= 0; i++)
+	{
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[i]);
+	}
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+this is all about static libraries projects.
