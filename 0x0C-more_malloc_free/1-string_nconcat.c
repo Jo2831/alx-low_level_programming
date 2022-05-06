8137b8fab075808ee5c88caed1fef99952a32877
Jo2831 <jo2831@github.com>
2022-05-06 05:20:29 -0800

    more_alloc_yosi

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,5 @@
-#include<stdlib.h>
+#include <stdlib.h>
+
 /**
  * string_nconcat - function that concatenates two strings
  *
@@ -12,34 +13,39 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-int i, j, l1, l2;
-char *ptr;
-i = j = l1 = l2 = 0;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-while (s1[l1] != '\0')
-l1++;
-while (s2[l2] != '\0')
-l2++;
-if (n >= 12)
-n = 12;
-ptr = (char *)malloc((n + l1 + 1) * sizeof(char));
-if (ptr == NULL)
-ptr = NULL;
-while (s1[i] != '\0')
-{
-ptr[i] = s1[i];
-i++;
-}
-while (j < n && s2[j] != '\0')
-{
-ptr[i] = s2[j];
-i++;
-j++;
-}
-ptr[i] = '\0';
-return (ptr);
+	unsigned int l1, l2, i, j;
+	char *str;
 
+	/*treat NULL as empty string*/
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	l1 = l2 = 0;
+	/*length of both s1 & s2 respectively*/
+	while (s1[l1] != '\0')
+		l1++;
+	while (s2[l2] != '\0')
+		l2++;
+
+	/*set n to length of s2*/
+	if (n >= l2)
+		n = l2;
+
+	str = (char *) malloc((l1 + n + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+
+	/*add s1 to str*/
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
+	/*add s2 n bytes to str*/
+	for (j = 0; j < n && s2[j] != '\0'; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
