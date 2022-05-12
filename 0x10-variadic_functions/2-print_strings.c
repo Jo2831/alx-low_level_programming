01a33bc92afab6da2f8e9125f760f6e93e763488
Jo2831 <jo2831@github.com>
2022-05-12 11:36:02 -0800

    variadic_functions__yosi

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - a function that prints strings, followed by a new line
+ *
+ * @separator: pointer to a constant separator
+ * @n: start of input variables
+ *
+ * Return: nothing
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list argc;
+	unsigned int i;
+	char *str;
+
+	va_start(argc, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(argc, char *);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+
+		if (separator && i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
