b3c76a5bb7c8cc882ad3b4b2e92a134af555e445
Jo2831 <jo2831@github.com>
2022-05-24 09:26:38 -0800

    more linked_list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,25 +1,31 @@
-#include"lists.h"
+#include "lists.h"
 
 /**
- *free_listint2 - remove datacode
+ * free_listint2 - a function that frees the list and sets head to NULL
  *
- *@head: pointer
+ * @head: pointer to the first node
  *
- *Return: void
- */
-
+ * Return: nothing
+*/
 void free_listint2(listint_t **head)
 {
-listint_t *node;
-node = *head;
-if (head == NULL)
-{
-return;
-}
-while (*head != NULL)
-{
-node = *head;
-*head = (*head)->next;
-free(node);
-}
+	listint_t *current, *temp;
+
+	if (head != NULL)
+	{
+		/*set head addr to current*/
+		current = *head;
+
+		/*iterate through the whole list*/
+		/*while setting the current node to temp*/
+		while ((temp = current) != NULL)
+		{
+			/*set next node to curretnt*/
+			current = current->next;
+			/*free temp, that is the current node*/
+			free(temp);
+		}
+
+		*head = NULL;
+	}
 }
