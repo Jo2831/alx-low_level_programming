3c3c96a3575666507dff2008481c556fbc5e1d92
Jo2831 <jo2831@github.com>
2022-07-29 08:50:35 -0800

    hash_table

---
+++ 0x1A-hash_tables/3-main.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	hash_table_t *ht;
+
+	ht = hash_table_create(1024);
+	hash_table_set(ht, "hetairas", "test");
+	hash_table_set(ht, "hetairas", "mentioner");
+
+	return (EXIT_SUCCESS);
+}
