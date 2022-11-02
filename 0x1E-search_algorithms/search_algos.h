b2c2a4d1777718ba4d8eb573fec0a7b7c43dd76e
Jo2831 <jo2831@github.com>
2022-11-02 09:59:28 -0800

    search_algorithms

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,4 +1,22 @@
-#include <stddef.h>
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/*
+ * File: search_algos.h
+ * Desc: This header file contains all the prototypes required for
+ * the 0x1E-search_algorithms project.
+ *
+ * Author: yoseph desalegn
+ * 
+ *
+ */
+#include <stdlib.h>
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+int jump_search(int *array, size_t size, int value);
+
+
+#endif
