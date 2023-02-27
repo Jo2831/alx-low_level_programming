cd02af0a27b1d6d7948563d1cb7155fff44e8cb9
Yoseph Tapano <yosephman76@gmail.com>
2023-02-27 19:32:29 +0300

    task4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,35 +1,21 @@
+#include "main.h"
+
 /**
- * reverse_array - a function that reverses the
- *                content of an array of integers
- *
- * @a: pointer to int array
- * @n: is the number of elements to swap
- *
- * Return: nothing
-*/
+ * reverse_array - reverses the given array
+ * @a: an array to be reversed
+ * @n: size of the array
+ */
 
 void reverse_array(int *a, int n)
 {
-	int jo, i, j;
+	int index;
+	int temp;
+	int i = n - 1;
 
-	i = 0;
-	j = n - 1;
-	/**
-	 * set value in array a in temp
-	 * then place the last array in
-	 * the first array then place
-	 * value in temp to last array
-	*/
-	while (i < j)
+	for (index = 0; index < n / 2; index++)
 	{
-		jo = a[i];
-		a[i] = a[j];
-		a[j] = jo;
-		i++;
-		j--;
+		temp = a[index];
+		a[index] = a[i - index];
+		a[i - index] = temp;
 	}
 }
-
-
-
-
