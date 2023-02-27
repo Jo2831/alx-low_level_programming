60d257f0efb814d41826bb70eed5fb6368c43dc7
Yoseph Tapano <yosephman76@gmail.com>
2023-02-27 20:14:31 +0300

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,37 +1,22 @@
+#include "main.h"
+
 /**
- * leet - a function that encodes a string into 1337
- *
- * @s: string input
- *
- * Return: @s
-*/
+ * leet - Encodes the given string to 1337 or eleet or leetspeak or leet
+ * @str: A string argument to be Encoded
+ * Return: A pointer that points to encoded string;
+ */
 
-char *leet(char *s)
+char *leet(char *str)
 {
-	int i, c = 0;
-	int sl[] = {97, 101, 111, 116, 108};
-	int ul[] = {65, 69, 79, 84, 76};
-	int n[] = {52, 51, 48, 55, 49};
+	int i, j;
+	char *map_leet = "ol?ea??t";
 
-	/*iterate values in array s*/
-	while (s[c] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		/**
-		 * loop through array value five times
-		 * to check if the value is equal to
-		 * any small letter in array sl or
-		 * upper letter in array ul and if it
-		 * is replace it with the value in array n
-		*/
-		for (i = 0; i < 5; i++)
-		{
-			if (s[c] == sl[i] || s[c] == ul[i])
-			{
-				s[c] = n[i];
-				break;
-			}
-		}
-		c++;
+		for (j = 0; j < 8; j++)
+			if (str[i] == map_leet[j] || str[i] + 32 == map_leet[j])
+				str[i] = j + '0';
 	}
-	return (s);
+
+	return (str);
 }
